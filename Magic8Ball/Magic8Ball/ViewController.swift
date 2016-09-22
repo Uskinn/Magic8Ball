051c2ab2848f2b6744cf5e586ce47dabcd6da234
Uskinn <s.nevzor@yahoo.com>
2016-09-21 23:44:32 -0400

    completed

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let 
+    let answers = ["Yes", "No", "Maybe", "Definitely", "Probably", "One Day"]
     
 
     @IBOutlet weak var answerLabel: UILabel!
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+      
+        if motion == .MotionShake {
+            let answerNumber = Int(arc4random_uniform(UInt32(answers.count)))
+            let answer = answers[answerNumber]
+            self.answerLabel.text = answer
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
