eb450802cc9c1d82d5042f300a4168dfa691933a
Uskinn <s.nevzor@yahoo.com>
2016-09-21 23:36:55 -0400

    storyboard completed

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Sergey Nevzorov on 9/21/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let 
+    
+
+    @IBOutlet weak var answerLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
